ec6cf62aaf559597f418465ea79c61e3c6b728d4
Ryan Atkinson <mail@ryanatkn.com>
2022-01-02 16:06:27 -0600

    shorten invitation

--- src/lib/app/PersonalStatement.svelte
+++ src/lib/app/PersonalStatement.svelte
@@ -7,10 +7,9 @@
 </p>
 
 <p>
-	If you would like to geek out, please reach out! I'm always looking for noncommercial
-	collaborators. I can't promise a response but I'll do my best: <a href="mailto:mail@ryanatkn.com"
-		>mail@ryanatkn.com</a
-	>. I'm also @ryanatkn on <a href="https://twitter.com/ryanatkn">Twitter</a> and
+	Want to geek out? Please reach out! I'm always looking for noncommercial collaborators. I can't
+	promise a response but I'll do my best: <a href="mailto:mail@ryanatkn.com">mail@ryanatkn.com</a>.
+	I'm also @ryanatkn on <a href="https://twitter.com/ryanatkn">Twitter</a> and
 	<a href="https://github.com/ryanatkn">GitHub</a>.
 </p>
 
