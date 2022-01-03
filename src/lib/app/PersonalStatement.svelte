48df6528101c05febb7884643b140b7a5b69eb43
Ryan Atkinson <mail@ryanatkn.com>
2022-01-03 00:10:22 -0600

    improve personal statement

--- src/lib/app/PersonalStatement.svelte
+++ src/lib/app/PersonalStatement.svelte
@@ -2,18 +2,82 @@
 	Welcome <span style="font-size: var(--font_size_xl5)">🐢</span>
 </h2>
 <p>
-	I'm a web developer interested in exploring and realizing more potential of collaborative digital
-	mediums for our own good.
+	I'm a web developer interested in exploring collaborative digital mediums and realizing more of
+	their human-and-humanity-helping potential.
 </p>
 
+<h2>Purpose:</h2>
+
+<p>This webpage has two primary purposes:</p>
+<ol>
+	<li>Convey basic public information about my work and work persona, like a resume or CV</li>
+	<li>Fish for possible collaborations</li>
+</ol>
 <p>
-	Want to geek out? Please reach out! I'm always looking for noncommercial collaborators. I can't
-	promise a response but I'll do my best: email <a href="mailto:mail@ryanatkn.com"
-		>mail@ryanatkn.com</a
-	>
-	& @ryanatkn on <a href="https://twitter.com/ryanatkn">Twitter</a> &
-	<a href="https://github.com/ryanatkn">GitHub</a>.
+	Want to geek out? Reach out~~ I'm always looking for noncommercial collaborations around the
+	topics and projects that interest me. I can't promise a response but I'll do my best:
 </p>
+<ul>
+	<li>
+		email me at <a href="mailto:mail@ryanatkn.com">mail@ryanatkn.com</a>
+	</li>
+	<li>
+		join me in the open source on <a href="https://github.com/ryanatkn">GitHub</a>, like the
+		<a href="https://github.com/spiderspace/spiderspace/discussions">spiderspace discussions</a> or
+		<a href="https://github.com/ryanatkn/cosmicplayground">cosmicplayground</a>
+	</li>
+	<li>
+		public social media on <a href="https://twitter.com/ryanatkn">Twitter</a>
+		<small>(TODO and Mastodon/Matrix?)</small>
+	</li>
+</ul>
+<p>
+	Collaborations can take many forms in many different spaces, public and private, and I'm open to
+	all of the noncommercial variety: if you read this document and have something in mind, I would
+	enjoy hearing about it, and if it's something in the open source, go do, don't ask for permission!
+	Some of my public collaborations include:
+</p>
+<ul>
+	<li><a href="https://github.com/ryanatkn">my GitHub repos</a> which are mostly web apps</li>
+	<li>
+		the
+		<a href="https://github.com/spiderspace/spiderspace/discussions">spiderspace discussions</a>
+		for web tech prototypes
+	</li>
+	<li>
+		the Felt.dev <a href="https://www.felt.dev/sketch/onboard/">Consentful Onboarding Sketch</a>
+		inspired by <a href="https://consentful.systems">consentful.systems</a>
+	</li>
+	<li>
+		<a href="https://www.cosmicplayground.org">cosmicplayground.org</a> for coding and refactoring a
+		web app with
+		<a href="https://svelte.dev">Svelte</a>, <a href="https://kit.svelte.dev">SvelteKit</a>, and
+		TypeScript
+	</li>
+</ul>
+
+<p>But why cooperate?</p>
+
+<ul>
+	<li>idk lol. do what u want be free~</li>
+	<li>
+		I can code and not everyone can or should. Little things like ideas are valued contributions
+	</li>
+	<li>teamwork makes the ___ work</li>
+	<li>some really fun/cool/useful/important things are possible only in groups</li>
+	<li>you can control how you're credited, or ghost along anonymously</li>
+	<li>
+		you prefer open source and research-oriented collabs over commercial ones (I work at <a
+			href="https://felt.social">@feltcoop</a
+		>)
+	</li>
+	<li>
+		Mozilla says: <em
+			>"The collective endowment of an exponentially growing, decentralized, global super-network
+			will always vastly exceed anything that can be created without it."</em
+		>
+	</li>
+</ul>
 
 <h2>I'm curious about:</h2>
 
@@ -31,21 +95,30 @@
 
 <ul>
 	<li>
-		How can the web improve our lives? How can technology improve our thinking and communication?
+		How can technology improve our lives? What roles should technology play to improve our thinking,
+		communication, and social environments? What else do we need to realize more of its liberatory
+		potential?
 	</li>
 	<li>
 		How can we design technology that respects and reinforces our personal consent boundaries?
 	</li>
 	<li>
-		How can we make software systems that afford programming powers in everyone's everyday life?
+		How can we include more people in designing, making, and sharing the building blocks of digital
+		mediums? How can we make software systems that afford programming powers in everyone's everyday
+		life? How far can we run with the web? How can we further leverage the web's capabilities?
 	</li>
 	<li>
-		What modes of cooperation are available in the meaningful aspects of life? How can technology
-		help?
+		What modes of cooperation are available to us in the various meaningful aspects of our lives?
+		How can technology help?
 	</li>
 	<li>
 		How can we play better games together? Examples? What are the lessons and how can we apply them
 		throughout everyday life? How can we make our worlds more safe, fair, and fun?
+	</li>
+	<li>
+		How can we implement things to realize our goals? In other words, what actions should we take?
+		Make apps? Publish research? Create audio and video content? Blogs? Which mediums do we want to
+		work in and why?
 	</li>
 </ul>
 
