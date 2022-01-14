ac4f85202f9b310f139b3f6bb9936b9f2bf37f9c
Ryan Atkinson <mail@ryanatkn.com>
2022-01-13 18:47:25 -0600

    add a question

--- src/lib/app/PersonalStatement.svelte
+++ src/lib/app/PersonalStatement.svelte
@@ -71,8 +71,8 @@
 	<li>
 		How can we include more people in designing, making, and sharing the building blocks of digital
 		mediums? How can we make software systems that afford programming powers in everyone's everyday
-		life? How can we further leverage the web's existing capabilities? Where is the web going and
-		how far can we run with it?
+		life? And where power is working for our collective and individual benefit? How can we further
+		leverage the web's existing capabilities? Where is the web going and how far can we run with it?
 	</li>
 	<li>
 		What modes of cooperation are available to us in the various meaningful aspects of our lives?
