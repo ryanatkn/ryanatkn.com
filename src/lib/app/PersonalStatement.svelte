7d2adbec48702adb75a5c6fd113fc024bd30b66e
Ryan Atkinson <mail@ryanatkn.com>
2022-02-21 21:24:55 -0600

    kind

--- src/lib/app/PersonalStatement.svelte
+++ src/lib/app/PersonalStatement.svelte
@@ -100,7 +100,7 @@
 		meaningful aspects of our lives? Which open source communities are thriving, and where is there
 		room for improvement? Who is being systemically excluded from participating, and what are the
 		best remedies? What are some optimistic visions of a vibrant and benevolent commons? How can we
-		make our communities and worlds more safe, welcoming, fair, fun, and fruitful?
+		make our communities and worlds more safe, welcoming, kind, fair, fun, and fruitful?
 	</li>
 </ul>
 
