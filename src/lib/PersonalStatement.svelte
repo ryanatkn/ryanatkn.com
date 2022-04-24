85cb712add9a0fc9a8e414da4255f4353a8612c0
Ryan Atkinson <mail@ryanatkn.com>
2022-04-24 13:46:44 -0500

    improvements

--- src/lib/PersonalStatement.svelte
+++ src/lib/PersonalStatement.svelte
@@ -8,9 +8,9 @@
 </blockquote>
 
 <p>
-	My main skill is coding frontend web apps, and I know enough backend and database stuff to make
-	frontends happen. I enjoy trying to make powerful tools that are easy to use, and I have a geeky
-	interest in user interfaces and social systems.
+	My main skill is coding frontend web apps, and I do just enough backend and database stuff to make
+	frontends. I enjoy trying to make powerful tools that are easy to use, and I have a geeky interest
+	in user interfaces and social systems.
 </p>
 
 <ul>
@@ -28,7 +28,8 @@
 
 <ul>
 	<li>
-		<a href="https://felt.social">@feltcoop</a> — a pre-alpha
+		<a href="https://felt.social">@feltcoop</a> — a
+		<a href="https://wikipedia.org/wiki/Software_release_life_cycle">pre-alpha</a>
 		<a href="https://platform.coop">platform co-op</a> making free and open source community
 		software (<a href="https://github.com/feltcoop">GitHub</a>,
 		<a href="https://twitter.com/feltcoop">Twitter</a>,
@@ -37,10 +38,15 @@
 	</li>
 	<li>
 		<a href="https://www.cosmicplayground.org">cosmicplayground.org</a>
-		— for fun and learning,
-		<em>"tools and toys for expanding minds"</em> (<a
-			href="https://github.com/ryanatkn/cosmicplayground">GitHub</a
-		>)
+		— hobby project for fun and learning,
+		<em>"tools and toys for expanding minds"</em>
+	</li>
+	<li>
+		@blitwagon — open source hobbyist persona at <a href="https://www.blitwagon.net"
+			>blitwagon.net</a
+		>
+		(<a href="https://github.com/blitwagon">GitHub</a>,
+		<a href="https://twitter.com/blitwagon">Twitter</a>)
 	</li>
 	<li>
 		<a href="https://www.spiderspace.org">spiderspace.org</a> — web prototypes that aren't ready for
@@ -48,117 +54,103 @@
 	</li>
 </ul>
 
-<h2>I'm curious about:</h2>
+<section>
+	<details>
+		<summary><h3>I'm curious about:</h3></summary>
 
-<ul>
-	<li>creative tools</li>
-	<li>community environments</li>
-	<li>crowd wisdom</li>
-	<li>participatory governance</li>
-	<li>distributed power</li>
-	<li>consentful design</li>
-	<li>networked trust and safety</li>
-	<li>small scale groups with strong bonds</li>
-	<li>large scale groups with scalable interactivity</li>
-	<li>how to bridge the technical and nontechnical worlds</li>
-	<li>how to build powerful user interfaces for working with information</li>
-</ul>
+		<ul>
+			<li>creative tools</li>
+			<li>community environments</li>
+			<li>crowd wisdom</li>
+			<li>participatory governance</li>
+			<li>distributed power</li>
+			<li>consentful design</li>
+			<li>networked trust and safety</li>
+			<li>small scale groups with strong bonds</li>
+			<li>large scale groups with scalable interactivity</li>
+			<li>how to bridge the technical and nontechnical worlds</li>
+			<li>how to build powerful user interfaces for working with information</li>
+		</ul>
+	</details>
 
-<h2>Questions I'm asking:</h2>
+	<details>
+		<summary><h3>Questions I'm asking:</h3></summary>
 
-<ul>
-	<li>
-		How can we design our technologies to bring out the best in ourselves? What roles can and should
-		technology play to improve our communication and relationships, health and wellbeing, and social
-		and physical ecologies? Are we building towards a more just world? Do our systems make us more
-		responsible for negative externalities? How can we ensure our efforts reduce inequalities and
-		increase agency? What are our blind spots, and what perspectives are we missing and minimizing?
-		What else do we need to realize more of technology's liberatory potential? What are the ripe
-		opportunities and low hanging fruit?
-	</li>
-	<li>
-		What are the interesting and useful frontiers of digital mediums? What fun creative tools can we
-		make? How can we include more people in designing, making, and sharing the building blocks of
-		digital mediums? How can we make software systems that afford programming powers in everyday
-		life for nontechnical and quasitechnical people? How can we further leverage the capabilities of
-		the web and our devices? Where is the web going and where should we contribute our time and
-		attention?
-	</li>
-	<li>
-		How can we grow sustainable and ever-evolving institutions to protect the things we care about?
-		How can we structure and develop organizations to achieve better coordinated outcomes while
-		supporting individual agency and autonomy? How can we build robust trusted networks? What forms
-		of participation work for the majority of us that aren't highly motivated? How can we all be
-		better informed with minimal effort? What tools can we design to improve collective decision
-		making and shared understanding? How can we make power dynamics in groups more explicit? What
-		does it mean for democratic community governance to have desirable dynamics around labor and
-		power? In what ways can we practice low-stakes democracy so we're better at it when the stakes
-		are higher?
-	</li>
-	<li>
-		How can we balance the needs of individuals and groups? When needs come into conflict, can we
-		find creative positive-sum solutions? Like, how can we all just get along and have nice things?
-		How can we design technology that respects and reinforces our personal consent boundaries? How
-		can we make social contracts more explicit and innovate new ones? Is our technology working for
-		our personal and collective benefit? If sometimes not, why not, and how can we fix it? What
-		false stories and invisible assumptions are blocking our imaginations?
-	</li>
-	<li>
-		What modes of cooperation are available to us where participation is positive sum,
-		nonfinancialized, and intrinsically motivated? Which open source communities are thriving, and
-		where is there room for improvement? How can we get more of them? Who is being systemically
-		excluded from participating, and what are the best remedies? What are some optimistic visions of
-		a vibrant and benevolent commons? How can we make our communities and worlds more safe,
-		welcoming, kind, fair, joyful, and adventuresome?
-	</li>
-</ul>
+		<ul>
+			<li>
+				How can we design our technologies to bring out the best in ourselves? What roles can and
+				should technology play to improve our communication and relationships, health and wellbeing,
+				and social and physical ecologies? Are we building towards a more just world? Do our systems
+				make us more responsible for negative externalities? How can we ensure our efforts reduce
+				inequalities and increase agency? What are our blind spots, and what perspectives are we
+				missing and minimizing? What else do we need to realize more of technology's liberatory
+				potential? What are the ripe opportunities and low hanging fruit?
+			</li>
+			<li>
+				What are the interesting and useful frontiers of digital mediums? What fun creative tools
+				can we make? How can we include more people in designing, making, and sharing the building
+				blocks of digital mediums? How can we make software systems that afford programming powers
+				in everyday life for nontechnical and quasitechnical people? How can we further leverage the
+				capabilities of the web and our devices? Where is the web going and where should we
+				contribute our time and attention?
+			</li>
+			<li>
+				How can we grow sustainable and ever-evolving institutions to protect the things we care
+				about? How can we structure and develop organizations to achieve better coordinated outcomes
+				while supporting individual agency and autonomy? How can we build robust trusted networks?
+				What forms of participation work for the majority of us that aren't highly motivated? How
+				can we all be better informed with minimal effort? What tools can we design to improve
+				collective decision making and shared understanding? How can we make power dynamics in
+				groups more explicit? What does it mean for democratic community governance to have
+				desirable dynamics around labor and power? In what ways can we practice low-stakes democracy
+				so we're better at it when the stakes are higher?
+			</li>
+			<li>
+				How can we balance the needs of individuals and groups? When needs come into conflict, can
+				we find creative positive-sum solutions? Like, how can we all just get along and have nice
+				things? How can we design technology that respects and reinforces our personal consent
+				boundaries? How can we make social contracts more explicit and innovate new ones? Is our
+				technology working for our personal and collective benefit? If sometimes not, why not, and
+				how can we fix it? What false stories and invisible assumptions are blocking our
+				imaginations?
+			</li>
+			<li>
+				What modes of cooperation are available to us where participation is positive sum,
+				nonfinancialized, and intrinsically motivated? Which open source communities are thriving,
+				and where is there room for improvement? How can we get more of them? Who is being
+				systemically excluded from participating, and what are the best remedies? What are some
+				optimistic visions of a vibrant and benevolent commons? How can we make our communities and
+				worlds more safe, welcoming, kind, fair, joyful, and adventuresome?
+			</li>
+		</ul>
+	</details>
 
-<h2>Related things I like:</h2>
+	<details>
+		<summary><h3>Related things I like:</h3></summary>
 
-<ul>
-	<li>free and open source software and data</li>
-	<li>
-		the web, the <a href="https://getdweb.net/principles/">DWeb Principles</a>, the
-		<a href="https://santaclaraprinciples.org">Santa Clara Principles</a>, and the
-		<a href="https://wikipedia.org/wiki/Rochdale_Principles">Rochdale Principles</a>
-	</li>
-	<li>
-		<a href="https://svelte.dev">Svelte</a> and
-		<a href="https://kit.svelte.dev">SvelteKit</a>, TypeScript, Node, Postgres
-	</li>
-	<li>
-		<a href="https://consentful.systems">consentful.systems</a> and
-		<a href="https://www.consentfultech.io">The Consentful Tech Project</a>
-	</li>
-	<li>
-		<a href="https://metagov.org">The Metagovernance Project</a>
-		and
-		<a href="https://communityrule.info">CommunityRule</a>
-	</li>
-</ul>
-<p>Some of my public collaborations include:</p>
-<ul>
-	<li>
-		<a href="https://github.com/ryanatkn">my GitHub repos</a> which are mostly web apps; I'm interested
-		in your input on any that aren't archived
-	</li>
-	<li>
-		the Felt.dev <a href="https://www.felt.dev/sketch/onboard/">Consentful Onboarding Sketch</a>
-		inspired by and in cooperation with
-		<a href="https://consentful.systems">consentful.systems</a>
-	</li>
-	<li>
-		<a href="https://www.cosmicplayground.org">cosmicplayground.org</a>
-		for coding and refactoring a web app with
-		<a href="https://svelte.dev">Svelte</a>,
-		<a href="https://kit.svelte.dev">SvelteKit</a>, and TypeScript
-	</li>
-	<li>
-		the
-		<a href="https://github.com/spiderspace/spiderspace/discussions">spiderspace discussions</a>
-		for web tech prototypes
-	</li>
-</ul>
+		<ul>
+			<li>free and open source software and data</li>
+			<li>
+				the web, the <a href="https://getdweb.net/principles/">DWeb Principles</a>, the
+				<a href="https://santaclaraprinciples.org">Santa Clara Principles</a>, and the
+				<a href="https://wikipedia.org/wiki/Rochdale_Principles">Rochdale Principles</a>
+			</li>
+			<li>
+				<a href="https://svelte.dev">Svelte</a> and
+				<a href="https://kit.svelte.dev">SvelteKit</a>, TypeScript, Node, Postgres
+			</li>
+			<li>
+				<a href="https://consentful.systems">consentful.systems</a> and
+				<a href="https://www.consentfultech.io">The Consentful Tech Project</a>
+			</li>
+			<li>
+				<a href="https://metagov.org">The Metagovernance Project</a>
+				and
+				<a href="https://communityrule.info">CommunityRule</a>
+			</li>
+		</ul>
+	</details>
+</section>
 
 <h2>Contact:</h2>
 <ul>
@@ -167,7 +159,7 @@
 	</li>
 	<li>
 		join me doing open source on <a href="https://github.com/ryanatkn">GitHub</a>, like
-		<a href="https://github.com/ryanatkn/cosmicplayground">cosmicplayground</a>
+		<a href="https://github.com/cosmicplayground/cosmicplayground">cosmicplayground</a>
 		or the
 		<a href="https://github.com/spiderspace/spiderspace/discussions">spiderspace discussions</a>
 		<small>(TODO and Felt once we figure out what that looks like)</small>
@@ -189,5 +181,12 @@
 		display: flex;
 		justify-content: center;
 		align-items: center;
+	}
+	summary h3 {
+		display: inline;
+		font-weight: 400;
+	}
+	section {
+		margin: var(--spacing_xl) 0 0 0;
 	}
 </style>
