b155380bd33592864c883bd214f7bb38fdb7c544
Ryan Atkinson <mail@ryanatkn.com>
2022-05-01 23:12:14 -0500

    finish

--- src/routes/blog/__layout.svelte
+++ src/routes/blog/__layout.svelte
@@ -13,12 +13,16 @@
 
 <style>
 	main {
+		min-height: 100%;
 		width: 100%;
 		position: relative;
 		/* TODO should these be on `.column` ? */
 		display: flex;
 		flex-direction: column;
 		align-items: center;
-		justify-content: center;
+		justify-content: flex-start;
+	}
+	.column {
+		flex: 1;
 	}
 </style>
--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -4,7 +4,10 @@
 </script>
 
 <div class="blog">
-	<LinkPath path={$page.url.pathname + '/0'} selectedPath={$page.url.pathname} />
+	<p>
+		<LinkPath path={$page.url.pathname + '/0'} selectedPath={$page.url.pathname}>🥬</LinkPath>
+	</p>
+	<h2><a href="/blog/0">What we're doing @feltcoop</a> <small>2022 May 1</small></h2>
 </div>
 
 <style>
@@ -12,6 +15,11 @@
 		display: flex;
 		flex-direction: column;
 		align-items: center;
+	}
+	p {
 		font-size: var(--font_size_xl3);
+	}
+	h2 {
+		margin-top: 0;
 	}
 </style>
