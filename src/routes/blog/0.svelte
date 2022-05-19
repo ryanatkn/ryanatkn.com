2933448991dcc46171206bbe09911bf8a6c4e139
Ryan Atkinson <mail@ryanatkn.com>
2022-05-19 18:46:02 -0500

    add a note about the team and coop

--- src/routes/blog/0.svelte
+++ src/routes/blog/0.svelte
@@ -227,7 +227,8 @@
 		In the not-too-distant future, I'm excited to find and fund three more colleagues and turn our <a
 			href="https://github.com/feltcoop/felt/blob/main/GOVERNANCE.md"
 			>Temporary Benevolent Dictatorship</a
-		> into a real co-op. (5 is the number required to form a Colorado cooperative)
+		> into a real co-op. (5 is the number required to form a Colorado cooperative; Hamilton lives there
+		and although we're a remote team, it has some of the best-developed cooperative laws in the country)
 	</p>
 	<p>
 		I would consider it a great success if one day, democratically selected experts make the
