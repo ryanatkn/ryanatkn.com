18c8de31c7bfe346b6f43f7b5d92a114a2f0c209
Ryan Atkinson <mail@ryanatkn.com>
2022-11-12 10:26:18 -0600

    update to @ryanatkn@toolsforthought.rocks

--- src/lib/PersonalStatement.svelte
+++ src/lib/PersonalStatement.svelte
@@ -82,8 +82,10 @@
 		<a href="mailto:mail@ryanatkn.com">mail@ryanatkn.com</a> (not looking for work)
 	</li>
 	<li>
-		public social media on <a href="https://twitter.com/ryanatkn">Twitter</a>,
-		<a rel="me" href="https://hachyderm.io/@ryanatkn">Mastodon</a>,
+		public social media on
+		<a rel="me" href="https://toolsforthought.rocks/@ryanatkn">@ryanatkn@toolsforthought.rocks</a>
+		on Mastodon and the fedi,
+		<a href="https://twitter.com/ryanatkn">Twitter</a>,
 		<a href="https://www.reddit.com/user/overtowed">Reddit</a>, and
 		<a href="https://lobste.rs/u/ryanatkn">Lobste.rs</a>
 		<small>(TODO and Matrix?)</small>
