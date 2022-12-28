79d0d4091a5612a6249cc583907a64ffd3909957
Ryan Atkinson <mail@ryanatkn.com>
2022-12-27 23:37:46 -0600

    add gen file

---
+++ src/lib/blog.gen.json.ts
@@ -0,0 +1,8 @@
+import type {Gen} from '@feltcoop/gro';
+
+// TODO BLOCK generate
+
+export const gen: Gen = async () => {
+	return `{"entries": ["/blog/a-year-of-making-open-source-web-community-software"]}
+  `;
+};
