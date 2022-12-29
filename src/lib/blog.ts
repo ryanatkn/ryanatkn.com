f8b1f87e4547cbab061c9f9bba4a426768297273
Ryan Atkinson <mail@ryanatkn.com>
2022-12-29 09:51:59 -0600

    todo

--- src/lib/blog.ts
+++ src/lib/blog.ts
@@ -1,6 +1,8 @@
 import type {FeedItemData} from '$lib/feed';
 import * as p0 from '../routes/blog/[slug]/0.svelte';
 
+// TODO generate this? how to include in builds?
+
 export const posts: FeedItemData[] = [p0.post];
 
 export const Components = [p0.default];
