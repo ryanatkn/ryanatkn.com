b73cf3a3856c1a75b73e412fc758ec65066ca848
Ryan Atkinson <mail@ryanatkn.com>
2022-12-29 11:05:28 -0600

    ok

---
+++ src/lib/blog.gen.ts
@@ -0,0 +1,49 @@
+import type {Gen} from '@feltcoop/gro';
+
+import {feedData} from '$lib/feedData';
+
+/* eslint-disable no-await-in-loop */
+
+// TODO refactor this to be reusable
+// TODO should this be `blog` or a more generic `feedData`?
+
+export const gen: Gen = async ({fs}) => {
+	const items: number[] = [];
+
+	let i = 0;
+	while (true) {
+		if (!(await fs.exists(`src/routes/blog/[slug]/${i}.svelte`))) {
+			break;
+		}
+		items.push(i);
+		i++;
+	}
+
+	return [
+		{
+			filename: './blog.ts',
+			content: `
+				import type {FeedItemData} from '$lib/feed';
+				${items.map((i) => `import * as p${i} from '../routes/blog/[slug]/${i}.svelte'`).join(';\n')};
+				
+				// TODO maybe export these zipped together into objects? currently have to match by index
+
+				export const posts: FeedItemData[] = [${items.map((i) => `p${i}.post`).join(', ')}];
+
+				export const Components = [${items.map((i) => `p${i}.default`).join(', ')}];
+			`,
+		},
+		{
+			filename: './blog.json',
+			content: JSON.stringify(feedData),
+		},
+		{
+			filename: './blog.json.d.ts',
+			content: `declare module '$lib/blog.json' {
+        import type {FeedData} from '$lib/feed';
+        const data: FeedData;
+        export default data;
+      }`,
+		},
+	];
+};
--- src/lib/blog.ts
+++ src/lib/blog.ts
@@ -1,7 +1,7 @@
 import type {FeedItemData} from '$lib/feed';
 import * as p0 from '../routes/blog/[slug]/0.svelte';
 
-// TODO BLOCK generate this? how to include in builds?
+// TODO maybe export these zipped together into objects? currently have to match by index
 
 export const posts: FeedItemData[] = [p0.post];
 
