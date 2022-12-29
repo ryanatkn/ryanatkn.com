0ea6e2800857022a6e84b3d333c09ea8196d972e
Ryan Atkinson <mail@ryanatkn.com>
2022-12-29 00:51:52 -0600

    wip

--- svelte.config.js
+++ svelte.config.js
@@ -5,6 +5,8 @@ import {readFileSync} from 'fs';
 // TODO import json directly when stable, don't want to deal with the warning for now
 // import blog from './src/lib/blog.json' assert {type: 'json'};
 const blog = JSON.parse(readFileSync('./src/lib/blog.json', 'utf8'));
+
+// TODO make generic
 const toPrerenderEntry = (url) => url.substring('https://www.ryanatkn.com'.length);
 
 /** @type {import('@sveltejs/kit').Config} */
