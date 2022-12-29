85e9ed7edb8ec495ac709536ee34dc1eaae4b47d
Ryan Atkinson <mail@ryanatkn.com>
2022-12-29 11:35:32 -0600

    refactor

--- src/lib/blog.gen.ts
+++ src/lib/blog.gen.ts
@@ -1,18 +1,22 @@
 import type {Gen} from '@feltcoop/gro';
+import {stripStart, stripEnd} from '@feltcoop/util/string.js';
 
 import {feed} from '../routes/blog/feed';
 
 /* eslint-disable no-await-in-loop */
 
-// TODO refactor this to be reusable
+// TODO refactor this to be reusable (see args below)
 // TODO should this be `blog` or a more generic `feedData`?
 
 export const gen: Gen = async ({fs}) => {
+	const TODO_get_from_args = 'blog/[slug]'; // TODO args? process.argv?
+	const path = stripStart(stripEnd(TODO_get_from_args, '/'), '/');
+
 	const items: number[] = [];
 
 	let i = 0;
 	while (true) {
-		if (!(await fs.exists(`src/routes/blog/[slug]/${i}.svelte`))) {
+		if (!(await fs.exists(`src/routes/${path}/${i}.svelte`))) {
 			break;
 		}
 		items.push(i);
@@ -24,7 +28,7 @@ export const gen: Gen = async ({fs}) => {
 			filename: './blog.ts',
 			content: `
 				import type {FeedItemData} from '$lib/feed';
-				${items.map((i) => `import * as p${i} from '../routes/blog/[slug]/${i}.svelte'`).join(';\n')};
+				${items.map((i) => `import * as p${i} from '../routes/${path}/${i}.svelte'`).join(';\n')};
 				
 				// TODO maybe export these zipped together into objects? currently have to match by index
 
