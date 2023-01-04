6eebdd380e20599c1d9fccb592dc3e0ff7bbe23f
Ryan Atkinson <mail@ryanatkn.com>
2023-01-04 02:22:02 -0600

    tweak blog post footer

--- src/lib/BlogPostFooter.svelte
+++ src/lib/BlogPostFooter.svelte
@@ -4,8 +4,16 @@
 	export let post: FeedItemData;
 </script>
 
-<p>
-	Want to talk about this post privately with me? <a
-		href="mailto:mail@ryanatkn.com?subject=Reply to post: {post.title}">↪ reply by email!</a
-	>
+<p>Want to talk about this post privately with me?</p>
+<p class="email">
+	<a href="mailto:mail@ryanatkn.com?subject=Reply to post: {post.title}">↪ reply by email 📬</a>
 </p>
+
+<style>
+	p {
+		text-align: center;
+	}
+	.email {
+		font-size: var(--font_size_xl);
+	}
+</style>
