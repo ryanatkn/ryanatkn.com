90de1d81f6a29de861d5ad233ffd947a07a9e20a
Ryan Atkinson <mail@ryanatkn.com>
2023-01-11 00:01:27 -0700

    toot

--- src/lib/blog.ts
+++ src/lib/blog.ts
@@ -1,8 +1,9 @@
 import type {FeedItemData} from '$lib/feed';
 import * as p0 from '../routes/blog/[slug]/0.svelte';
+import * as p1 from '../routes/blog/[slug]/1.svelte';
 
 // TODO maybe export these zipped together into objects? currently have to match by index
 
-export const posts: FeedItemData[] = [p0.post];
+export const posts: FeedItemData[] = [p0.post, p1.post];
 
-export const Components = [p0.default];
+export const Components = [p0.default, p1.default];
---
+++ src/routes/blog/[slug]/1.svelte
@@ -0,0 +1,119 @@
+<script lang="ts" context="module">
+	import type {FeedItemData} from '$lib/feed';
+
+	export const post: FeedItemData = {
+		id: 'https://www.ryanatkn.com/blog/1',
+		title: 'toot',
+		url: 'https://www.ryanatkn.com/blog/toot',
+		date_published: '2023-01-11T07:00:08.049Z',
+		date_modified: '2023-01-11T07:00:08.049Z',
+		summary: 'toot',
+		tags: ['toot'],
+	};
+</script>
+
+<script lang="ts">
+	import {writable} from 'svelte/store';
+
+	const dunbars_number = writable(144);
+</script>
+
+<p>1d ryanatkn@hci.social Ryan Atkinson @ryanatkn@hci.social</p>
+
+<p>
+	this idea has been going around a lot lately with recent advancements in #ai and it's not a big
+	leap from where we are today: imagine a social media app where you get lots of likes and replies
+	to your stuff but it's almost all bots designed to monetize your attention
+</p>
+
+<p>imagine that it's _you and your loved ones_ stuck in that matrix without knowing it</p>
+
+<p>
+	humanity and trust matter more than ever, and our culture and tools need to catch up
+	ryanatkn@hci.social Ryan Atkinson @ryanatkn@hci.social
+</p>
+
+<p>"humanity and trust matter more than ever"</p>
+
+<p>
+	hm does it actually matter more today than in the past? maybe ya kinda? thinking about the tight
+	knit groups of about {$dunbars_number} (yes I made the reference lol) tribespeople we evolved in, and
+	how detached I/family/friends are from that tribal mutual aid oxytocin cocktail, in our modern hyperconnected
+	hyperaliented hyperworld
+</p>
+
+<p>
+	my mind just panics and blurts out "free and open source software and data" and then
+	thought-terminatingly, "governance" Jan 10, 2023, 22:23 · · 0 · 0 1h ryanatkn@hci.social Ryan
+	Atkinson @ryanatkn@hci.social
+</p>
+
+<p>
+	the thing is with that cocktail, tmaoc, it's not hard to be a good person, just don't be fucked up
+	and you can function as a happy tribe when times are good, if you have a culture that enforces
+	accountability for behavior, hopefully with positive psychology and good vibes
+</p>
+
+<p>
+	we couldn't possibly have evolved with the dysfunctional societal relationships we see dominate
+	the planet today
+</p>
+
+<p>scale did us in</p>
+
+<p>
+	we could intentionally form groups at every scale, then #interconnect#interoperate#interdepend#
+	13m ryanatkn Ryan Atkinson @ryanatkn
+</p>
+
+<p>@ryanatkn@hci.social</p>
+
+<p>and when bad times?</p>
+<p>ChatGPT continues:</p>
+
+<p>
+	But even in bad times, there is always hope. We can always find ways to come together and support
+	one another, to find strength in our shared humanity and trust. It may not be easy, but together,
+	we can rise above any challenges and build a better future for ourselves and those we love. So let
+	us not give in to fear or despair, but instead hold on to hope and kindness, and work towards
+	building a world that is truly just and equitable for all. 6m ryanatkn Ryan Atkinson @ryanatkn
+</p>
+
+<p>@ryanatkn@hci.social</p>
+
+<p>the hyperconnected hyperaliented hyperworld aka hhh</p>
+
+<p>(ChatGPT takes over here)</p>
+
+<p>
+	is a double-edged sword. On one hand, it allows us to connect with people from all over the globe,
+	expand our perspectives, and access a wealth of knowledge. But on the other hand, it can lead to
+	feelings of isolation, manipulation, and a loss of privacy.
+</p>
+
+<p>1/3 4m ryanatkn Ryan Atkinson @ryanatkn</p>
+
+<p>@ryanatkn@hci.social</p>
+
+<p>ChatGPT continues:</p>
+
+<p>
+	To counter this, we need to actively work on building trust and creating a sense of community
+	within our online interactions. This means not only being mindful of our own behavior and the way
+	we treat others, but also supporting open-source and transparent technologies that give us control
+	over our data and online presence.
+</p>
+
+<p>2/3 4m ryanatkn Ryan Atkinson @ryanatkn</p>
+<p>@ryanatkn@hci.social</p>
+
+<p>ChatGPT concludes:</p>
+
+<p>
+	It also means actively working towards forming interdependent and interconnected communities,
+	rather than relying on a centralized authority to govern us. By coming together, we can create a
+	new culture and set of tools that prioritize humanity and trust over profit and manipulation.
+	Let's strive to make the hhh a place where we can all thrive.
+</p>
+
+<p>3/3</p>
