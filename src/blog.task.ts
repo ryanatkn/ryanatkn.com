647eefaa266bfd0cba7f1594a184588de30b3c4b
Ryan Atkinson <mail@ryanatkn.com>
2023-01-21 13:58:27 -0700

    gen

--- src/blog.task.ts
+++ src/blog.task.ts
@@ -3,20 +3,21 @@ import {z} from 'zod';
 import {stripStart, stripEnd} from '@feltcoop/util/string.js';
 import {formatFile} from '@feltcoop/gro/dist/format/formatFile.js';
 
+// TODO probably source this from `gro.config.ts` or other config
+const DEFAULT_URL = 'https://www.ryanatkn.com/blog';
+
 const Args = z
 	.object({
-		url: z
-			.string({description: 'full URL to the blog post directory'})
-			.default('https://www.ryanatkn.com/blog'),
+		url: z.string({description: 'url to the blog post directory'}).default(DEFAULT_URL),
 		date: z.string({description: "the post's date_published"}).default(new Date().toISOString()),
 	})
 	.strict();
 type Args = z.infer<typeof Args>;
 
 export const task: Task<Args> = {
-	summary: 'add initial dataset to the the database',
+	summary: 'create a new blog post',
 	Args,
-	run: async ({args, fs}) => {
+	run: async ({args, fs, log, invokeTask}) => {
 		const {url, date} = args;
 
 		const {origin, pathname} = new URL(url);
@@ -37,31 +38,32 @@ export const task: Task<Args> = {
 		}
 
 		const unformatted = `
-      <script lang="ts" context="module">
-        import type {FeedItemData} from '$lib/feed';
-
-        export const post: FeedItemData = {
-          id: '${origin}/${path}/${index}',
-          title: 'Some title todo',
-          url: '${origin}/${path}/some-title-todo',
-          date_published: '${date}',
-          date_modified: '${date}',
-          summary: 'todo',
-          tags: ['todo'],
-        };
-      </script>
-
-      <script lang="ts">
-        import {base} from '$app/paths';
-      </script>
-
-      <p>
-        <a href="{base}/a/b/c">a local link</a>
-      </p>
-    `;
+			<script lang="ts" context="module">
+				import type {FeedItemData} from '$lib/blog/blogFeed';
+				export const post: FeedItemData = {
+					id: '${origin}/${path}/${index}',
+					title: 'Some title todo',
+					url: '${origin}/${path}/some-title-todo',
+					date_published: '${date}',
+					date_modified: '${date}',
+					summary: 'todo',
+					tags: ['todo'],
+				};
+			</script>
+			<script lang="ts">
+				import {base} from '$app/paths';
+			</script>
+			<p>
+				<a href="{base}/a/b/c">a local link</a>
+			</p>
+		`;
 		const formatted = await formatFile(fs, postPath, unformatted);
 
 		await fs.writeFile(postPath, formatted);
+
+		await invokeTask('gen');
+
+		log.info(`created empty blog post with index ${index} at ${postPath}`);
 	},
 };
 
