a7498f61e35408f6c13c51a285b872b46bc75d65
Ryan Atkinson <mail@ryanatkn.com>
2023-07-09 15:11:13 -0600

    clean up prerender entries

---
+++ src/routes/blog/[slug]/+page.ts
@@ -0,0 +1,8 @@
+import {readFileSync} from 'node:fs';
+import type {EntryGenerator} from './$types';
+
+export const entries: EntryGenerator = () => {
+	const blog = JSON.parse(readFileSync('./src/lib/blog.json', 'utf8'));
+	console.log(`blog`, blog.entries);
+	return blog.entries;
+};
