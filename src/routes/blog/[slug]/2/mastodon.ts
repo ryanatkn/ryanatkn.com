0f339f3b8eeef834ee07ab95f7f3575b9736c213
Ryan Atkinson <mail@ryanatkn.com>
2023-07-19 01:48:26 -0600

    wip

--- src/routes/blog/[slug]/2/mastodon.ts
+++ src/routes/blog/[slug]/2/mastodon.ts
@@ -1,5 +1,10 @@
 import type {Post} from '$lib/post';
 
+/**
+ * https://docs.joinmastodon.org/entities/Context/
+ *
+ * https://:domain/api/v1/statuses/:id/context
+ */
 export interface MastodonContext {
 	ancestors: Post[];
 	descendants: Post[];
