b7a7b72536e8a632e02906cac6127573c58fcc75
Ryan Atkinson <mail@ryanatkn.com>
2023-07-22 12:23:42 -0700

    text

--- src/lib/FeedItemDate.svelte
+++ src/lib/FeedItemDate.svelte
@@ -7,5 +7,5 @@
 
 <span>{formatDate(item.date_published || item.date_modified)}</span
 >{#if item.date_published && item.date_modified && item.date_published !== item.date_modified}<span
-		>updated {formatDate(item.date_modified)}</span
+		style:padding-left="var(--spacing_sm)">updated {formatDate(item.date_modified)}</span
 	>{/if}
