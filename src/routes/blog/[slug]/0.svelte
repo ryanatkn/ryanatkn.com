017b58f24febcff3c999241da68dcc39bf2e5716
Ryan Atkinson <mail@ryanatkn.com>
2023-07-23 12:44:14 -0600

    wip

--- src/routes/blog/[slug]/0.svelte
+++ src/routes/blog/[slug]/0.svelte
@@ -260,7 +260,7 @@
 		<a href="https://ui.felt.dev/sketch/onboard">Consentful Onboarding Sketch</a>.
 	</p>
 	<p>
-		Our perspective as toolmakers is that we'll attempt to support a wide range of possibilities,
+		Our perspective as toolmakers is that we'll attempt to support all the things people want to do,
 		from traditional moderator setups to democratic control and much more, and provide templates and
 		guidance to help communities achieve their goals. We'll give this a lot of attention before
 		beta, but I suspect the good stuff is years away, maybe using standards that
@@ -272,7 +272,7 @@
 	<h2>Tech notes</h2>
 	<p>
 		We tried to be thoughtful about our tech stack to be friendly to beginners without compromising
-		on the UX we deliver. We chose the web because we think it's the best way to deliver the UX we
+		on the delivered UX. We chose the web because we think it's the best way to deliver the UX we
 		want to the most people, and we chose JS, Node.js, and npm because of fit, familiarity, and
 		productivity.
 	</p>
@@ -288,25 +288,26 @@
 	<p>
 		The web is everywhere and has a lot of unrealized potential. In combination, Node.js, Postgres,
 		nginx, and Linux are all productive and capable of delivering a good UX. TypeScript, Svelte,
-		SvelteKit, and Vite are fantastic for making UIs. We don't yet make
-		<a
-			href="https://github.com/feltjs/felt-server/blob/main/src/docs/known-issues.md#not-using-sveltekits-server-side-data-loading"
-			>full use of SvelteKit's features</a
-		>, but we feel productive and literally delighted working with it.
+		SvelteKit, and Vite are fantastic for making UIs.
 	</p>
 	<h3>customizable and extensible</h3>
 	<p>
-		Both the server and (future) clients are open source and our APIs are open, so clients can be
-		modified or created from scratch. We use TypeScript and Svelte across the full stack, and we try
-		to leverage the benefits of integration when available, like sharing types, helpers, and
-		schemas. We'll try to maximize the freedoms of users and developers, but security and
-		performance are unfortunate constraints and sometimes buzzkills :\
+		Both the server and clients are open source and our APIs are open, so clients can be modified
+		with low friction or created from scratch. (our client will be decoupled from the server during
+		alpha, stay tuned) We use TypeScript and Svelte across the full stack, and we try to leverage
+		the benefits of integration when available, like sharing types, helpers, and schemas. We'll try
+		to maximize the freedoms of users and developers, but security and performance are unfortunate
+		constraints and sometimes buzzkills :\
 	</p>
 	<p>
 		Our client has a <a href="https://wikipedia.org/wiki/Client_(computing)#Thick">thickness</a>
 		optimized for a powerful and low-latency UX over long sessions, which means a lot of JS and client-side
 		caching, and thanks to SvelteKit we should be able to deliver good experiences in many cases with
-		fast loadtimes and minimal or zero JS.
+		fast loadtimes and minimal or zero JS. (but we're
+		<a
+			href="https://github.com/feltjs/felt-server/blob/main/src/docs/known-issues.md#not-using-sveltekits-server-side-data-loading"
+			>not there yet</a
+		>)
 	</p>
 	<h3>scaling (and not)</h3>
 	<p>
