e5017283a3efcfd44235eec19bb62dda183526d4
Ryan Atkinson <mail@ryanatkn.com>
2023-07-24 15:23:25 -0600

    more

--- src/routes/blog/[slug]/+page.ts
+++ src/routes/blog/[slug]/+page.ts
@@ -1,10 +1,12 @@
-import {readFileSync} from 'node:fs';
 import type {EntryGenerator} from './$types';
+import blog_entries from '$lib/blog_entries.json';
 
-export const entries: EntryGenerator = () => {
-	const blog = JSON.parse(readFileSync('./src/lib/blog.json', 'utf8'));
-	console.log(`blog`, blog.entries);
-	return blog.entries;
+export const entries: EntryGenerator = async () => {
+	console.log(`blog_entries`, blog_entries.entries);
+	return blog_entries.entries.map((e) => {
+		const parts = e.split('/');
+		return {slug: parts[parts.length - 1]};
+	});
 };
 
 export const prerender = true; // TODO is this needed?
