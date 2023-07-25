55ac9b9535da472233b7a8c5b5b8011cdded6a5e
Ryan Atkinson <mail@ryanatkn.com>
2023-07-24 23:54:04 -0600

    clean up prerender entries (#11)

---
+++ src/lib/blog_entries.json.d.ts
@@ -0,0 +1,4 @@
+declare module '$lib/blog_entries.json' {
+	const data: {entries: string[]};
+	export default data;
+}
--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,5 @@
 import {typescript} from 'svelte-preprocess-esbuild';
 import adapter from '@sveltejs/adapter-static';
-import {readFileSync} from 'fs';
-
-// TODO import json directly when stable, don't want to deal with the warning for now
-// import blog from './src/lib/blog.json' assert {type: 'json'};
-let blog;
-try {
-	blog = JSON.parse(readFileSync('./src/lib/blog.json', 'utf8'));
-} catch (err) {}
 
 /** @type {import('@sveltejs/kit').Config} */
 export default {
@@ -17,9 +9,5 @@ export default {
 		adapter: adapter(),
 		files: {assets: 'src/static'},
 		alias: {$routes: 'src/routes'},
-		// TODO is very messy and error prone, didn't used to need to enumerate the paths like this (blog.entries used to be enough before an upgrade)
-		prerender: blog
-			? {entries: ['/', '/about', '/blog', '/influences'].concat(blog.entries)}
-			: undefined,
 	},
 };
