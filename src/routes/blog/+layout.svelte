e71d19cd588f63eafcfb3d8ae431451e3d17ce22
Ryan Atkinson <mail@ryanatkn.com>
2023-07-26 20:00:04 -0600

    padding

--- src/routes/blog/+layout.svelte
+++ src/routes/blog/+layout.svelte
@@ -17,7 +17,7 @@
 <div class="breadcrumbs">
 	<Breadcrumbs><Favicon /></Breadcrumbs>
 </div>
-<div class="wrapper padded_md">
+<div class="wrapper">
 	{#if is_blog_post}
 		<Blog {feed}>
 			<slot />
@@ -36,6 +36,7 @@
 		flex-direction: column;
 		align-items: center;
 		flex: 1;
+		padding: var(--spacing_xl2);
 	}
 	.breadcrumbs {
 		margin-top: var(--spacing_xl3);
