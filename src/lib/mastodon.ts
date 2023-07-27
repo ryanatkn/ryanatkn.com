ddd4f8bdd8920669069c30cf985237f62bbb1755
Ryan Atkinson <mail@ryanatkn.com>
2023-07-26 22:00:05 -0600

    wip

--- src/lib/mastodon.ts
+++ src/lib/mastodon.ts
@@ -30,6 +30,118 @@ export const fetch_data = async (url: string): Promise<any | null> => {
 	}
 };
 
+// TODO BLOCK name
+export const to_status_url = (host: string, id: string): string =>
+	`https://${host}/api/v1/statuses/${id}`;
+
+export const to_status_context_url = (host: string, id: string): string =>
+	`https://${host}/api/v1/statuses/${id}/context`;
+
+export const to_status_url_with_author = (host: string, author: string, id: string): string =>
+	`https://${host}/@${author}/${id}`;
+
+export const to_favourites_url = (host: string, id: string): string =>
+	`https://${host}/api/v1/statuses/${id}/favourited_by`;
+
+export const to_api_url = (
+	url: string | undefined,
+	host: string | undefined,
+	id: string | undefined,
+): string | null => {
+	if (!url && !host && !id) {
+		throw new Error('either url or host+id must be provided');
+	}
+	return url || (host && id ? to_status_context_url(host, id) : null);
+};
+
+// TODO BLOCK rename with the above
+
+export const to_post_url = (api_url: string | null): string | null => {
+	if (!api_url) return null;
+	const parsed = parse_status_url_with_author(api_url);
+	if (!parsed) return null;
+	parsed.host, parsed.id;
+	return to_status_url_with_author(parsed.host, parsed.author, parsed.id);
+};
+
+/**
+ * Parses a url to a post or API endpoint for a post, aka Mastodon status context.
+ * @param url
+ * @returns the parsed host and id params, if any
+ */
+export const parse_status_context_url = (url: string): MastodonStatusContextParams | null => {
+	try {
+		const u = new URL(url);
+		const parts = stripEnd(u.pathname, '/context').split('/');
+		return {
+			host: u.host,
+			id: parts[parts.length - 1],
+		};
+	} catch (err) {
+		return null;
+	}
+};
+
+/**
+ * Parses a url to a post or API endpoint for a post, aka Mastodon status context.
+ * @param url
+ * @returns the parsed host and id params, if any
+ */
+export const parse_status_url_with_author = (url: string): MastodonPostParams | null => {
+	try {
+		const u = new URL(url);
+		const parts = stripEnd(u.pathname, '/context').split('/');
+		const author = parts[0][0] === '@' ? parts[0].substring(1) : null; // eslint-disable-line @typescript-eslint/prefer-string-starts-ends-with
+		if (!author) return null;
+		const id = parts.length > 1 ? parts[parts.length - 1] : null;
+		if (!id) return null;
+		return {host: u.host, author, id};
+	} catch (err) {
+		return null;
+	}
+};
+
+// TODO BLOCK implement for direct links
+export const fetch_status_context_by_url = async (url: string): Promise<MastodonContext | null> => {
+	const parsed = parse_status_context_url(url);
+	if (!parsed) return null;
+	const {host, id} = parsed;
+	return fetch_status_context(host, id);
+};
+
+// TODO BLOCK go through a single fetch helper and trace each call to the API,
+// so we can see the history in a tab displayed to any users who want to dig
+
+export const fetch_status_context = async (
+	host: string,
+	id: string,
+): Promise<MastodonContext | null> => {
+	const u = to_status_context_url(host, id);
+	return fetch_data(u);
+};
+
+export const fetch_status = async (host: string, id: string): Promise<MastodonStatus | null> => {
+	const u = to_status_url(host, id);
+	return fetch_data(u);
+};
+
+// TODO BLOCK implement for direct links
+export const fetch_status_by_url = async (url: string): Promise<MastodonStatus | null> => {
+	const parsed = parse_status_context_url(url);
+	if (!parsed) return null;
+	const {host, id} = parsed;
+	const u = to_status_url(host, id);
+	return fetch_data(u);
+};
+
+export const fetch_favourites = async (
+	host: string,
+	status: MastodonStatus,
+): Promise<MastodonFavourites[] | null> => {
+	const u = to_favourites_url(host, status.id);
+	return fetch_data(u);
+};
+
 /**
  * https://docs.joinmastodon.org/entities/Context/
  *
@@ -146,115 +258,3 @@ export interface MastodonPostParams {
 // const parse_mastodon_status = parse(MASTODON_STATUS_PATH);
 
 export type Url = Flavored<string, 'Url'>;
-
-// TODO BLOCK name
-export const to_status_url = (host: string, id: string): string =>
-	`https://${host}/api/v1/statuses/${id}`;
-
-export const to_status_context_url = (host: string, id: string): string =>
-	`https://${host}/api/v1/statuses/${id}/context`;
-
-export const to_status_url_with_author = (host: string, author: string, id: string): string =>
-	`https://${host}/@${author}/${id}`;
-
-export const to_favourites_url = (host: string, id: string): string =>
-	`https://${host}/api/v1/statuses/${id}/favourited_by`;
-
-export const to_api_url = (
-	url: string | undefined,
-	host: string | undefined,
-	id: string | undefined,
-): string | null => {
-	if (!url && !host && !id) {
-		throw new Error('either url or host+id must be provided');
-	}
-	return url || (host && id ? to_status_context_url(host, id) : null);
-};
-
-// TODO BLOCK rename with the above
-
-export const to_post_url = (api_url: string | null): string | null => {
-	if (!api_url) return null;
-	const parsed = parse_status_url_with_author(api_url);
-	if (!parsed) return null;
-	parsed.host, parsed.id;
-	return to_status_url_with_author(parsed.host, parsed.author, parsed.id);
-};
-
-/**
- * Parses a url to a post or API endpoint for a post, aka Mastodon status context.
- * @param url
- * @returns the parsed host and id params, if any
- */
-export const parse_status_context_url = (url: string): MastodonStatusContextParams | null => {
-	try {
-		const u = new URL(url);
-		const parts = stripEnd(u.pathname, '/context').split('/');
-		return {
-			host: u.host,
-			id: parts[parts.length - 1],
-		};
-	} catch (err) {
-		return null;
-	}
-};
-
-/**
- * Parses a url to a post or API endpoint for a post, aka Mastodon status context.
- * @param url
- * @returns the parsed host and id params, if any
- */
-export const parse_status_url_with_author = (url: string): MastodonPostParams | null => {
-	try {
-		const u = new URL(url);
-		const parts = stripEnd(u.pathname, '/context').split('/');
-		const author = parts[0][0] === '@' ? parts[0].substring(1) : null; // eslint-disable-line @typescript-eslint/prefer-string-starts-ends-with
-		if (!author) return null;
-		const id = parts.length > 1 ? parts[parts.length - 1] : null;
-		if (!id) return null;
-		return {host: u.host, author, id};
-	} catch (err) {
-		return null;
-	}
-};
-
-// TODO BLOCK implement for direct links
-export const fetch_status_context_by_url = async (url: string): Promise<MastodonContext | null> => {
-	const parsed = parse_status_context_url(url);
-	if (!parsed) return null;
-	const {host, id} = parsed;
-	return fetch_status_context(host, id);
-};
-
-// TODO BLOCK go through a single fetch helper and trace each call to the API,
-// so we can see the history in a tab displayed to any users who want to dig
-
-export const fetch_status_context = async (
-	host: string,
-	id: string,
-): Promise<MastodonContext | null> => {
-	const u = to_status_context_url(host, id);
-	return fetch_data(u);
-};
-
-export const fetch_status = async (host: string, id: string): Promise<MastodonStatus | null> => {
-	const u = to_status_url(host, id);
-	return fetch_data(u);
-};
-
-// TODO BLOCK implement for direct links
-export const fetch_status_by_url = async (url: string): Promise<MastodonStatus | null> => {
-	const parsed = parse_status_context_url(url);
-	if (!parsed) return null;
-	const {host, id} = parsed;
-	const u = to_status_url(host, id);
-	return fetch_data(u);
-};
-
-export const fetch_favourites = async (
-	host: string,
-	status: MastodonStatus,
-): Promise<MastodonFavourites[] | null> => {
-	const u = to_favourites_url(host, status.id);
-	return fetch_data(u);
-};
