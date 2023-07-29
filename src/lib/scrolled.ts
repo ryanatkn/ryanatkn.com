957bdec5106a98e0e05005b5fcd03880ffc0e3ed
Ryan Atkinson <mail@ryanatkn.com>
2023-07-29 14:09:57 -0600

    wip

--- src/lib/scrolled.ts
+++ src/lib/scrolled.ts
@@ -1,5 +1,6 @@
 import type {Action} from 'svelte/action';
 
+// TODO improve this so it can fire every time it enters/exits, not just the first enter
 // TODO name? scrolled, onscreen, scrolled_onscreen
 
 export const scrolled: Action<Element, () => void> = (el, params) => {
