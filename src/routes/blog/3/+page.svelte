107f8a37f3b39c81f4a9b58ed0a885ed7da83539
Ryan Atkinson <mail@ryanatkn.com>
2023-08-01 15:00:17 -0600

    wip

--- src/routes/blog/3/+page.svelte
+++ src/routes/blog/3/+page.svelte
@@ -232,8 +232,8 @@
 			Here's <a href="https://github.com/ryanatkn/ryanatkn.com/pull/12/files">the GitHub PR</a>.
 		</p>
 		<p>
-			To avoid some possible security and privacy issues from injecting Mastodon's arbitrary HTML
-			into the page like I'm doing, I added a fairly restrictive <a
+			To avoid some possible security and privacy issues from injecting Mastodon's HTML into the
+			page like I'm doing, I added a fairly restrictive <a
 				href="https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP">content security policy</a
 			>. It disallows arbitrary third party requests, so it may break if my Mastodon host
 			hachyderm.io changes how it loads resources. If something goes wrong or changes, having icons
