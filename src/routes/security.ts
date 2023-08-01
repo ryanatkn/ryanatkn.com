bdb995b97441a782f7a6efe591b57db3f689a376
Ryan Atkinson <mail@ryanatkn.com>
2023-08-01 13:22:51 -0600

    word

--- src/routes/security.ts
+++ src/routes/security.ts
@@ -1,5 +1,6 @@
 import {dev} from '$app/environment';
 
+// TODO maybe use `https://kit.svelte.dev/docs/configuration#csp`, but then can't be dynamic?
 export const prod_content_security_policy =
 	"default-src 'unsafe-inline' 'self'; connect-src 'self' https://hachyderm.io/; img-src 'self' https://hachyderm.io/ https://*.hachyderm.io/;";
 
