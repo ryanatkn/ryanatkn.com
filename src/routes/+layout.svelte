7ff3f5676c3fe8440821dc580360c077cdd0781b
Ryan Atkinson <mail@ryanatkn.com>
2023-08-06 21:37:42 -0600

    wip

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import '@feltjs/felt-ui/style.css';
 	import '@feltjs/felt-ui/theme.css';
-	import '@feltjs/felt-ui/prism.css';
 	import '$routes/style.css';
 
 	import Themed from '@feltjs/felt-ui/Themed.svelte';
