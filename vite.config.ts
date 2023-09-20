9388ff3d03388a890ac4bbb20ef52b100a1062ad
Ryan Atkinson <mail@ryanatkn.com>
2023-09-20 16:01:48 -0600

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import {sveltekit} from '@sveltejs/kit/vite';
 
 const config: UserConfig = {
 	plugins: [sveltekit()],
-	ssr: {noExternal: ['@feltjs/felt-ui']}, // TODO was using this instead of `optimizeDeps` but needed to switch, see that comment
+	ssr: {noExternal: ['@feltjs/felt-ui']},
 };
 
 export default config;
