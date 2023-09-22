429e35a5266f698af9ac7fa9658d8f77f29ee76a
Ryan Atkinson <mail@ryanatkn.com>
2023-09-22 09:09:32 -0600

    wip

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -2,7 +2,7 @@
 	import Blog from '$lib/Blog.svelte';
 	import {feed} from '$routes/blog/feed';
 
-	// TODO load data with SvelteKit correctly so `feed.posts`
+	// TODO load data with SvelteKit correctly so `feed.posts` is lazy-loaded and
 	// doesn't include imported components for each of the blog post `+page.svelte` files
 </script>
 
