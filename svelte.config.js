e5ebca5752132ab0176f4dd556f4bf85d585113f
Ryan Atkinson <mail@ryanatkn.com>
2023-09-27 22:27:04 -0600

    wip

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,14 @@
 //@ts-expect-error
 import {typescript} from 'svelte-preprocess-esbuild';
-import static_adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 export default {
 	preprocess: typescript(),
 	compilerOptions: {immutable: true},
-	vitePlugin: {inspector: true},
+	vitePlugin: {inspector: true}, // docs: https://github.com/sveltejs/vite-plugin-svelte/blob/main/docs/inspector.md
 	kit: {
-		adapter: static_adapter(),
+		adapter: adapter(),
 		files: {assets: 'src/static'},
 		alias: {$routes: 'src/routes', $fixtures: 'src/fixtures'},
 	},
