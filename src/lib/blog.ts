a17633e39d4f9f506810ed2bc35a6734c4eeac69
Ryan Atkinson <mail@ryanatkn.com>
2023-10-17 20:51:30 -0600

    wip

--- src/lib/blog.ts
+++ src/lib/blog.ts
@@ -1,6 +1,6 @@
 import type {FeedItemData} from '$lib/feed';
-import {post as p1} from '$routes/blog/1/post';
-import {post as p2} from '$routes/blog/2/post';
-import {post as p3} from '$routes/blog/3/post';
+import {post as p1} from '$routes/blog/1/+page.svelte';
+import {post as p2} from '$routes/blog/2/+page.svelte';
+import {post as p3} from '$routes/blog/3/+page.svelte';
 
 export const posts: FeedItemData[] = [p1, p2, p3];
--- src/routes/blog/2/+page.svelte
+++ src/routes/blog/2/+page.svelte
@@ -1,3 +1,18 @@
+<script lang="ts" context="module">
+	import type {FeedItemData} from '$lib/feed';
+
+	export const post: FeedItemData = {
+		id: 'https://www.ryanatkn.com/blog/2',
+		title: 'Modeling virtual social spaces: in this house we post cat pics on Saturday',
+		url: 'https://www.ryanatkn.com/blog/modeling-virtual-social-spaces-in-this-house-we-post-cat-pics-on-saturday',
+		date_published: '2023-01-22T21:44:17.224Z',
+		date_modified: '2023-01-28T17:20:51.259Z',
+		summary:
+			'Proposing a model for one of the central aspects of online communities: the virtual social space.',
+		tags: ['web', 'community', 'social media', 'design', 'technology', 'software'],
+	};
+</script>
+
 <script lang="ts">
 	import {base} from '$app/paths';
 </script>
