ef1f30a9428bef120d557cfaf7cc7118d9e52947
Ryan Atkinson <mail@ryanatkn.com>
2023-10-17 20:52:26 -0600

    wip

--- src/lib/blog_components.ts
+++ src/lib/blog_components.ts
@@ -1,6 +1,2 @@
 // TODO this file shouldn't exist, change to SvelteKit load?
-import s1 from '../routes/blog/1/+page.svelte';
-import s2 from '../routes/blog/2/+page.svelte';
-import s3 from '../routes/blog/3/+page.svelte';
-
-export const components = [s1, s2, s3];
+export const components = [];
