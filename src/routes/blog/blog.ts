90a495f35b0cb09f31c4334447795437e05dc16d
Ryan Atkinson <mail@ryanatkn.com>
2024-04-12 18:01:53 -0600

    wip

--- src/routes/blog/blog.ts
+++ src/routes/blog/blog.ts
@@ -2,7 +2,7 @@ import type {Blog_Feed_Data} from '$lib/blog.js';
 
 // TODO extract to fuz.config.ts
 export const blog: Blog_Feed_Data = {
-	title: 'Ryan Atkinson',
+	title: "Ryan Atkinson's blog",
 	id: 'https://www.ryanatkn.com/',
 	home_page_url: 'https://www.ryanatkn.com/',
 	description: 'blog of a web developer building free and open source tools and toys',
