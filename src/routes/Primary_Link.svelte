9af18822153e50d36a58c01475acd5095ef14c00
Ryan Atkinson <mail@ryanatkn.com>
2024-04-15 12:42:14 -0600

    fix primary link

--- src/routes/Primary_Link.svelte
+++ src/routes/Primary_Link.svelte
@@ -12,12 +12,16 @@
 
 	const {attrs, children}: Props = $props();
 
+	// TODO upstream to belt?
+	const pop_path = (pathname: string): string =>
+		pathname.substring(0, pathname.lastIndexOf('/')) || '/';
+
 	// TODO the `strip_start` usage is a hack to get SSR working the same as CSR,
 	// `base` is `'.'` in SSR but `''` in CSR?
 	const b = strip_start(base, '.');
 	const pathname = $derived($page.url.pathname);
 	const at_root_path = $derived(pathname === b + '/');
-	const href = $derived(at_root_path ? `${b}/blog` : `${b}/`);
+	const href = $derived(at_root_path ? `${b}/blog` : pop_path(pathname));
 </script>
 
 <a
