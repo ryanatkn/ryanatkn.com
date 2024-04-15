5233d9a1dbfce1847961049a7109fe9103e0749e
Ryan Atkinson <mail@ryanatkn.com>
2024-04-15 13:08:33 -0600

    add blog page link

--- src/routes/Page_Links.svelte
+++ src/routes/Page_Links.svelte
@@ -10,6 +10,10 @@
 {#if !pathname.endsWith('/about')}
 	<Card href="{base}/about" icon="🪶" align="right">about</Card>
 {/if}
+<!-- TODO this isn't correct either -->
+{#if !pathname.endsWith('/blog') && !pathname.includes('/blog/')}
+	<Card href="{base}/blog" icon="🪧">blog</Card>
+{/if}
 {#if !pathname.endsWith('/funding')}
 	<Card href="{base}/funding" icon="🐚">funding</Card>
 {/if}
