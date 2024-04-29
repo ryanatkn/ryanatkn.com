d7787e3a6cfdca03729ef65140a87780835f744c
Ryan Atkinson <mail@ryanatkn.com>
2024-04-29 09:42:24 -0600

    wip

--- src/routes/Me.svelte
+++ src/routes/Me.svelte
@@ -2,8 +2,6 @@
 	import {base} from '$app/paths';
 </script>
 
-<!-- TODO what is this error? -->
-<!-- svelte-ignore a11y-img-redundant-alt -->
 <img
 	src="{base}/me.png"
 	alt="picture of me smiling on a couch in a blue shirt while holding my sister's calico cat Caramel who's out of frame"
