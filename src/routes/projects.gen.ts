a6820c2a139596d6876b636186e89b2d48a30f08
Ryan Atkinson <mail@ryanatkn.com>
2024-07-05 08:07:43 -0600

    wip

--- src/routes/projects.gen.ts
+++ src/routes/projects.gen.ts
@@ -21,10 +21,21 @@ export const gen: Gen = ({origin_id}) => {
 
 interface Project_Metadata {
 	name: string;
+	/**
+	 * Allows HTML.
+	 */
+	description?: string;
+	motto?: string; // TODO @multiple this is a hack because cosmicplayground hasn't been deployed
 	repo?: string;
+	/**
+	 * Allows HTML.
+	 */
 	title: string;
+	/**
+	 * Allows HTML.
+	 */
 	subtitle?: string;
-	logo?: string; // TODO @multiple this is a hack because cosmicplayground hasn't been deployed with the logo field
+	logo?: string; // TODO @multiple this is a hack because cosmicplayground hasn't been deployed
 	logo_style?: string;
 }
 
@@ -39,24 +50,28 @@ const projects_metadata: Project_Metadata[] = [
 	{
 		name: 'github',
 		title: '<a href="https://github.com/ryanatkn">github.com/ryanatkn</a>',
-		subtitle: 'my free and open source software',
+		description: 'my free and open source software',
 	},
 	// {
 	// 	name: 'spiderspace',
 	//  repo: '@ryanatkn/spiderspace.org',
 	// 	title: '<a href="https://www.spiderspace.org/">spiderspace.org</a>',
+	// 	description: 'community website with a YouTube channel about building it',
 	// 	subtitle: `<a class="chip" href="https://www.youtube.com/@spiderspace_8000">YouTube</a> <a class="chip" href="https://github.com/spiderspace/spiderspace.org">source</a>`,
 	// },
 	{
 		name: 'webdevladder.net',
 		repo: 'webdevladder.net',
 		title: '<a href="https://www.webdevladder.net/">webdevladder.net</a>',
-		subtitle: `<a class="chip" href="https://www.youtube.com/@webdevladder">YouTube</a> <a class="chip" href="https://github.com/ryanatkn/webdevladder.net">source</a>`,
+		description:
+			'a YouTube channel and blog for realworld webdev with <a href="https://typescriptlang.org/">TypeScript</a> and <a href="https://svelte.dev/">Svelte</a>',
+		subtitle: `<a class="chip" href="https://www.youtube.com/@webdevladder">channel</a> <a class="chip" href="https://www.youtube.com/@webdevladder_vods">vods</a> <a class="chip" href="https://github.com/ryanatkn/webdevladder.net">source</a>`,
 	},
 	{
 		name: 'zzz',
 		repo: '@ryanatkn/zzz',
 		title: '<a href="https://zzz.ryanatkn.com/">Zzz</a>',
+		description: '(not yet published) social web app framework',
 		subtitle: `<a class="chip" href="https://github.com/ryanatkn/zzz">source</a>`, // TODO publish zzz - <a class="chip" href="https://www.npmjs.com/package/@ryanatkn/zzz">npm</a>
 	},
 	{
@@ -69,21 +84,26 @@ const projects_metadata: Project_Metadata[] = [
 		name: 'fuz',
 		repo: '@ryanatkn/fuz',
 		title: '<a href="https://www.fuz.dev/">Fuz</a>',
+		description: '<a href="https://svelte.dev/">Svelte</a> UI library',
 		subtitle: `<a class="chip" href="https://github.com/ryanatkn/fuz">source</a> <a class="chip" href="https://www.npmjs.com/package/@ryanatkn/fuz">npm</a>`,
 	},
 	{
 		name: 'gro',
 		repo: '@ryanatkn/gro',
 		title: '<a href="https://github.com/ryanatkn/gro">Gro</a>',
+		description:
+			'task runner and toolkit extending <a href="https://kit.svelte.dev/">SvelteKit</a>',
 		subtitle: `<a class="chip" href="https://github.com/ryanatkn/gro">source</a> <a class="chip" href="https://www.npmjs.com/package/@ryanatkn/gro">npm</a>`,
 	},
 	{
 		name: 'cosmicplayground',
 		repo: '@ryanatkn/cosmicplayground', // TODO change to `cosmicplayground.org`
-		title: '<a href="https://www.cosmicplayground.org">cosmicplayground.org</a>',
+		title: '<a href="https://www.cosmicplayground.org">cosmicplayground.org</a> 🌌', // TODO @multiple this is a hack because cosmicplayground hasn't been deployed (remove glyph)
 		subtitle: '<a class="chip" href="https://github.com/ryanatkn/cosmicplayground">source</a>',
+		description: 'hobby project for fun and learning', // TODO @multiple this is a hack because cosmicplayground hasn't been deployed
+		motto: 'tools and toys for expanding minds', // TODO @multiple this is a hack because cosmicplayground hasn't been deployed
 		logo_style: 'border-radius: 50%',
-		logo: 'favicon.png', // TODO @multiple this is a hack because cosmicplayground hasn't been deployed with the logo field
+		logo: 'favicon.png', // TODO @multiple this is a hack because cosmicplayground hasn't been deployed
 	},
 ];
 
@@ -97,9 +117,9 @@ const projects: Project_Info[] = projects_metadata.map((project_metadata) => {
 		description,
 		homepage,
 		motto,
-		logo = project_metadata.logo, // TODO @multiple this is a hack because cosmicplayground hasn't been deployed with the logo field
+		logo = project_metadata.logo, // TODO @multiple this is a hack because cosmicplayground hasn't been deployed
 		logo_alt,
 		glyph,
 	} = (deployment as Fetched_Deployment).package_json; // TODO fix type to avoid casting
-	return {...project_metadata, description, homepage, motto, logo, logo_alt, glyph};
+	return {description, homepage, motto, logo, logo_alt, glyph, ...project_metadata};
 });
