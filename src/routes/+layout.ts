f4070d46ebec5f255c192ddf60878fc57b83e31c
Ryan Atkinson <mail@ryanatkn.com>
2025-05-06 21:54:12 -0600

    upgrade deps and disable ssr for prod

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,3 +1,6 @@
 export const prerender = true;
 // TODO fixes CSP error for SSR that otherwise requires `'script-src-attr': ['unsafe-inline']` - https://github.com/sveltejs/kit/issues/11747
+// disable SSR in production when fixed
 export const ssr = false;
+// import {DEV} from 'esm-env';
+// export const ssr = DEV;
