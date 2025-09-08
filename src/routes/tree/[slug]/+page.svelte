3114a76281de33e3a5b1979622b99061c075b8ad
Ryan Atkinson <mail@ryanatkn.com>
2025-09-08 08:25:17 -0400

    wip

--- src/routes/tree/[slug]/+page.svelte
+++ src/routes/tree/[slug]/+page.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
-	import {page} from '$app/state';
 	import Tree_Item_Page from '@ryanatkn/fuz_gitops/Tree_Item_Page.svelte';
 	import {repos_context} from '@ryanatkn/fuz_gitops/repo.js';
 
 	// TODO ideally there would be one `Packages_Tree` mounted by the layout
 
+	const {params} = $props();
+
 	const {repo, repos} = repos_context.get();
 
-	const {slug} = $derived(page.params);
+	const {slug} = $derived(params);
 </script>
 
 <svelte:head>
