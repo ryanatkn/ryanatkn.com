bd8565b9eefdf5e294918c4c6a82364abc762f3f
Ryan Atkinson <mail@ryanatkn.com>
2025-11-26 08:59:52 -0500

    wip

--- src/routes/tree/+layout.svelte
+++ src/routes/tree/+layout.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import {repos_parse, repos_context} from '@ryanatkn/fuz_gitops/repo.svelte.js';
+	import {Repo, repos_parse, repos_context} from '@ryanatkn/fuz_gitops/repo.svelte.js';
 	import type {Snippet} from 'svelte';
 
 	import {repos_json} from '$routes/repos.ts';
@@ -12,7 +12,7 @@
 
 	// this is duplicated in each repos page to code split the repos data
 
-	repos_context.set(repos_parse(repos_json, 'https://www.ryanatkn.com/'));
+	repos_context.set(repos_parse(repos_json.map((r) => new Repo(r)), 'https://www.ryanatkn.com/'));
 </script>
 
 <div class="width_100 flex_1">
--- src/routes/tree/[slug]/+page.ts
+++ src/routes/tree/[slug]/+page.ts
@@ -1,9 +1,9 @@
 import type {EntryGenerator} from './$types.js';
-import {repos_parse} from '@ryanatkn/fuz_gitops/repo.svelte.js';
+import {Repo, repos_parse} from '@ryanatkn/fuz_gitops/repo.svelte.js';
 
 import {repos_json} from '$routes/repos.ts';
 
-const parsed = repos_parse(repos_json, 'https://www.ryanatkn.com/');
+const parsed = repos_parse(repos_json.map((r) => new Repo(r)), 'https://www.ryanatkn.com/');
 
 export const entries: EntryGenerator = () => {
 	return parsed.repos.map((d) => ({slug: d.library.repo_name}));
