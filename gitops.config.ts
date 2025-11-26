d5982b8730bd610264d9325d848da86d132037fe
Ryan Atkinson <mail@ryanatkn.com>
2025-11-26 08:50:30 -0500

    wip

--- gitops.config.ts
+++ gitops.config.ts
@@ -3,21 +3,24 @@ import type {CreateGitopsConfig} from '@ryanatkn/fuz_gitops/gitops_config.js';
 const config: CreateGitopsConfig = () => {
 	return {
 		repos: [
-			'https://github.com/ryanatkn/zzz',
+			// 'https://github.com/ryanatkn/zzz',
 			'https://github.com/ryanatkn/moss',
 			'https://github.com/ryanatkn/fuz',
 			'https://github.com/ryanatkn/gro',
 			'https://github.com/ryanatkn/belt',
-			'https://github.com/ryanatkn/fuz_template',
-			'https://github.com/ryanatkn/fuz_blog',
-			'https://github.com/ryanatkn/fuz_mastodon',
-			'https://github.com/ryanatkn/fuz_code',
+			// 'https://github.com/ryanatkn/fuz_template',
+			// 'https://github.com/ryanatkn/fuz_blog',
+			// 'https://github.com/ryanatkn/fuz_mastodon',
+			// 'https://github.com/ryanatkn/fuz_code',
 			// 'https://github.com/ryanatkn/fuz_gitops',
-			'https://github.com/ryanatkn/webdevladder.net',
+			// 'https://github.com/ryanatkn/webdevladder.net',
 			// 'https://github.com/ryanatkn/ztack.net',
 			// 'https://github.com/ryanatkn/spiderspace.org',
 			// 'https://github.com/ryanatkn/grogarden.org',
-			// 'https://github.com/ryanatkn/ryanatkn.com',
+			{
+				repo_url: 'https://github.com/ryanatkn/ryanatkn.com',
+				branch: 'upgrade-deps',
+			},
 		],
 	};
 };
