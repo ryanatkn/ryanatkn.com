22d16ef527bd54ba22904660ab5872ee28e82c95
Ryan Atkinson <mail@ryanatkn.com>
2026-09-16 13:11:05 -0400

    deps: upgrade gro and fuz_util, rename blake3-wasm from blake3_wasm

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,5 @@ import { vite_plugin_pkg_json } from '@fuzdev/fuz_ui/vite_plugin_pkg_json.ts';
 
 export default defineConfig({
 	plugins: [sveltekit(), svelte_docinfo(), vite_plugin_fuz_css(), vite_plugin_pkg_json()],
-	optimizeDeps: { exclude: ['@fuzdev/blake3_wasm'] }
+	optimizeDeps: { exclude: ['@fuzdev/blake3-wasm'] }
 });
